2869801e80ac5ec08446dcf48f1f4d00b99eeebd
JohnScarrow <johna@scarrows.com>
2017-04-19 18:05:35 -0700

    needs local storeage

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 'use script';
 
-var i, j, k;
 
+var clicksRemaining = 5;
+var app=document.getElementById('app');
+var i, j, k, l, h;
+var toPush =[];
 function pictureGen(picture, name, fileName) {
   this.pictureNum = picture;
   this.name = name;
@@ -9,11 +12,11 @@ function pictureGen(picture, name, fileName) {
   this.clicks = 0;
   this.timesDisplayed = 0;
 }
-var ImagesInSecondToLastScreen = [];
 var ImagesOnLastScreen = [];
 var ImagesOnScreen = [];
 var nextImage = [];
-var imagesForHtml = [
+var currImage =[];
+var busMallProducts = [
   new pictureGen(1, 'bag', './bag.jpg'),
   new pictureGen(2, 'banana', './banana.jpg'),
   new pictureGen(3, 'bathroom', './bathroom.jpg'),
@@ -38,46 +41,187 @@ var imagesForHtml = [
   new pictureGen(20, 'water-can', './water-can.jpg'),
   new pictureGen(21, 'wine-glass', './wine-glass.jpg')
 ];
+
+try {
+  busMallProducts = JSON.parse(localStorage.myStorage);
+} catch (error) {
+  console.log(error);
+}
+var imagesForHtml = busMallProducts;
 function randImg(){
-  return Math.floor(Math.random()*(imagesForHtml.length));
+  return Math.floor(Math.random()*(busMallProducts.length));
 }
 function getThreeRandomImages(){
-  imagesForHtml = imagesForHtml.concat(ImagesInSecondToLastScreen);
-  ImagesInSecondToLastScreen = ImagesOnLastScreen;
-  ImagesOnLastScreen = ImagesOnScreen;
-  ImagesOnScreen = [];
-  nextImage = imagesForHtml.splice(randImg(), 1);
-  ImagesOnScreen =ImagesOnScreen.concat(nextImage);
-  nextImage = imagesForHtml.splice(randImg(), 1);
-  ImagesOnScreen =ImagesOnScreen.concat(nextImage);
-  nextImage = imagesForHtml.splice(randImg(), 1);
-  ImagesOnScreen =ImagesOnScreen.concat(nextImage);
-  return ImagesOnScreen;
+  //curPush = [];
+  function curPush() {
+    if(currImage[0] == prevImage[0]|| currImage[1] == prevImage[1] || currImage[2] == prevImage[2]){
+      i = randImg();
+      currImage.push(i);
+      for (j = i; j == i; l){
+        j = randImg();
+      }
+      currImage.push(j);
+      for (k=i; k == i || k == j; l){
+        k = randImg();
+      }
+      currImage.push(k);
+    }else{
+      currImage=[];
+      getThreeRandomImages();
+    }
 }
-pictureGen.prototype.AddTimesDisplayed = function () {
-  return this.timesDisplayed++;
-};
+  var prevImage = currImage;
+  currImage = [];
+  currImage.push();
+  curPush();
+  console.log('current ', currImage);
+  console.log('previous ', prevImage);
+}
+  // imagesForHtml = imagesForHtml.concat(ImagesOnLastScreen);
+  // ImagesOnLastScreen = ImagesOnScreen;
+  // ImagesOnScreen = [];
+  // nextImage = imagesForHtml.splice(randImg(), 1);
+  // ImagesOnScreen =ImagesOnScreen.concat(nextImage);
+  // nextImage = imagesForHtml.splice(randImg(), 1);
+  // ImagesOnScreen =ImagesOnScreen.concat(nextImage);
+  // nextImage = imagesForHtml.splice(randImg(), 1);
+  // ImagesOnScreen =ImagesOnScreen.concat(nextImage);
 
-getThreeRandomImages();
-function newRandom(){
 
-  var threeOnScreen;
-  i = 0;
-  threeOnScreen=document.getElementById('app');
-  threeOnScreen.textContent ='';
-  for (i = 0; i < 3; i++){
-    var newImage = new  Image(300, 300);
-    newImage.src = ImagesOnScreen[i].img;
-    threeOnScreen.appendChild(newImage);
-    ImagesOnScreen[i].AddTimesDisplayed();
-    getThreeRandomImages();
+//getThreeRandomImages();
+renderPhotoChoices();
+// function newRandom(){
+//   clicksRemaining--;
+//   i = 0;
+//   app.textContent ='';
+//   for (i = 0; i < 3; i++){
+//     var newImage = new  Image(300, 300);
+//     newImage.src = ImagesOnScreen[i].img;
+//     newImage.setAttribute('img', i);
+//     app.appendChild(newImage);
+//     ImagesOnScreen[i].AddTimesDisplayed();
+//     getThreeRandomImages();
+//   }
+//   return app;
+// }
+//newRandom();
+
+function handleImageClick(event) {
+  //
+  console.log ('test');
+  var choice = event.target;
+  var photoIndex = choice.getAttribute('img');
+  ImagesOnScreen[photoIndex].clicks++;
+  console.log('Clicked', ImagesOnScreen[photoIndex]);
+
+
+  clicksRemaining--;
+  console.log('clicksRemaining', clicksRemaining);
+  if (clicksRemaining > 0){
+    renderPhotoChoices();
+  }  else {
+    reassembleChart();
   }
-  return threeOnScreen;
 }
-newRandom();
+
+function renderPhotoChoices(){
+  getThreeRandomImages();
+  app.textContent = '';
+  var imageElement;
+  for(var i=0; i < ImagesOnScreen.length; i++){
+    ImagesOnScreen[i].timesDisplayed++;
+    imageElement = document.createElement('img');
+    imageElement.src = ImagesOnScreen[i].img;
+    imageElement.setAttribute('img', i);
+    imageElement.addEventListener('click', handleImageClick);
+    app.appendChild(imageElement);
+  }
+}
 
 
+function reassembleChart(){
+  // refill photos array
+  imagesForHtml = imagesForHtml.concat(ImagesOnScreen);
+  imagesForHtml = imagesForHtml.concat(ImagesOnLastScreen);
 
+  app.textContent = '';
+
+  var canvas = document.createElement('canvas');
+  canvas.width = app.clientWidth;
+  canvas.height = app.clientWidth;
+  app.appendChild(canvas);
+
+  var ctx = canvas.getContext('2d');
+
+  var data = {
+    labels: [],
+    datasets: [
+      {
+        label: 'clicks',
+        data: [],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      },
+      {
+        label: 'displayed',
+        data: [],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      },
+    ],
+  };
+
+  var currentPhoto;
+  for(var i=0; i< imagesForHtml.length; i++){
+    currentPhoto = imagesForHtml[i];
+    data.labels.push(currentPhoto.name);
+    data.datasets[0].data.push(currentPhoto.clicks);
+    data.datasets[1].data.push(currentPhoto.timesDisplayed);
+  }
+
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: data,
+  });
+}
+busMallProducts = imagesForHtml;
+try {
+  console.log('busmall', busMallProducts);
+  localStorage.myStorage = JSON.stringify(busMallProducts);
+} catch (error) {
+  console.log(error);
+}
 
 // }
 // function tripleGen(){
