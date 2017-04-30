82d479ddb4699190b5983da483aa7bf3f106134f
JohnScarrow <johna@scarrows.com>
2017-04-30 14:52:34 -0700

    fixed errors noticed by ta

--- app.js
+++ app.js
@@ -8,39 +8,37 @@ var prevImage;
 function pictureGen(picture, name, fileName) {
   this.pictureNum = picture;
   this.name = name;
-  this.img = fileName;
+  this.img = './assets' + fileName;
   this.clicks = 0;
   this.timesDisplayed = 0;
 }
-var ImagesOnLastScreen = [];
 var ImagesOnScreen = [];
 var currImage =[];
-// var toPush =[];
-// var nextImage = [];
+
 var busMallProducts = [
-  new pictureGen(1, 'bag', './bag.jpg'),
-  new pictureGen(2, 'banana', './banana.jpg'),
-  new pictureGen(3, 'bathroom', './bathroom.jpg'),
-  new pictureGen(5, 'boots', './boots.jpg'),
-  new pictureGen(6, 'breakfast', './breakfast.jpg'),
+  new pictureGen(1, 'bag', '/bag.jpg'),
+  new pictureGen(2, 'banana', '/banana.jpg'),
+  new pictureGen(3, 'bathroom', '/bathroom.jpg'),
+  new pictureGen(5, 'boots', '/boots.jpg'),
+  new pictureGen(6, 'breakfast', '/breakfast.jpg'),
 
-  new pictureGen(7, 'bubblegum', './bubblegum.jpg'),
-  new pictureGen(8, 'chair', './chair.jpg'),
-  new pictureGen(9, 'cthulhu', './cthulhu.jpg'),
-  new pictureGen(10, 'dog-duck', './dog-duck.jpg'),
-  new pictureGen(11, 'dragon', './dragon.jpg'),
+  new pictureGen(7, 'bubblegum', '/bubblegum.jpg'),
+  new pictureGen(8, 'chair', '/chair.jpg'),
+  new pictureGen(9, 'cthulhu', '/cthulhu.jpg'),
+  new pictureGen(10, 'dog-duck', '/dog-duck.jpg'),
+  new pictureGen(11, 'dragon', '/dragon.jpg'),
 
-  new pictureGen(12, 'pen', './pen.jpg'),
-  new pictureGen(13, 'pet-sweep', './pet-sweep.jpg'),
-  new pictureGen(14, 'scissors', './scissors.jpg'),
-  new pictureGen(15, 'shark', './shark.jpg'),
-  new pictureGen(16, 'sweep', './sweep.png'),
+  new pictureGen(12, 'pen', '/pen.jpg'),
+  new pictureGen(13, 'pet-sweep', '/pet-sweep.jpg'),
+  new pictureGen(14, 'scissors', '/scissors.jpg'),
+  new pictureGen(15, 'shark', '/shark.jpg'),
+  new pictureGen(16, 'sweep', '/sweep.png'),
 
-  new pictureGen(17, 'tauntaun', './tauntaun.jpg'),
-  new pictureGen(18, 'unicorn', './unicorn.jpg'),
-  new pictureGen(19, 'usb', './usb.gif'),
-  new pictureGen(20, 'water-can', './water-can.jpg'),
-  new pictureGen(21, 'wine-glass', './wine-glass.jpg')
+  new pictureGen(17, 'tauntaun', '/tauntaun.jpg'),
+  new pictureGen(18, 'unicorn', '/unicorn.jpg'),
+  new pictureGen(19, 'usb', '/usb.gif'),
+  new pictureGen(20, 'water-can', '/water-can.jpg'),
+  new pictureGen(21, 'wine-glass', '/wine-glass.jpg')
 ];
 
 try {
@@ -48,7 +46,6 @@ try {
 } catch (error) {
   console.log(error);
 }
-//var imagesForHtml = busMallProducts;
 function randImg(){
   return Math.floor(Math.random()*(busMallProducts.length));
 }
@@ -62,11 +59,6 @@ function curPush() {
     j = randImg();
     k = randImg();
   }
-  // while (j == i){
-  //   j = randImg();
-  // }
-  // while (k == i && k == j)
-  //   k = randImg();
 }
 
 function prevCheck(){
@@ -83,7 +75,6 @@ function getThreeRandomImages(){
   currImage = [];
   curPush();
   prevCheck();
-  // currImage.push();
   currImage.push(i);
   currImage.push(j);
   currImage.push(k);
@@ -95,36 +86,8 @@ function getThreeRandomImages(){
 
 }
 
-
-
-  // imagesForHtml = imagesForHtml.concat(ImagesOnLastScreen);
-  // ImagesOnLastScreen = ImagesOnScreen;
-  // ImagesOnScreen = [];
-  // nextImage = imagesForHtml.splice(randImg(), 1);
-  // ImagesOnScreen =ImagesOnScreen.concat(nextImage);
-  // nextImage = imagesForHtml.splice(randImg(), 1);
-  // ImagesOnScreen =ImagesOnScreen.concat(nextImage);
-  // nextImage = imagesForHtml.splice(randImg(), 1);
-  // ImagesOnScreen =ImagesOnScreen.concat(nextImage);
-
-
-//getThreeRandomImages();
 renderPhotoChoices();
-// function newRandom(){
-//   clicksRemaining--;
-//   i = 0;
-//   app.textContent ='';
-//   for (i = 0; i < 3; i++){
-//     var newImage = new  Image(300, 300);
-//     newImage.src = ImagesOnScreen[i].img;
-//     newImage.setAttribute('img', i);
-//     app.appendChild(newImage);
-//     ImagesOnScreen[i].AddTimesDisplayed();
-//     getThreeRandomImages();
-//   }
-//   return app;
-// }
-//newRandom();
+
 
 
 function handleImageClick(event) {
@@ -133,7 +96,7 @@ function handleImageClick(event) {
   var choice = event.target;
   var photoIndex = choice.getAttribute('img');
   ImagesOnScreen[photoIndex].clicks++;
-  console.log('Clicked', ImagesOnScreen[photoIndex]);
+  console.log('Clicked ', ImagesOnScreen[photoIndex]);
 
 
   clicksRemaining--;
@@ -162,9 +125,6 @@ function renderPhotoChoices(){
 
 
 function reassembleChart(){
-  // refill photos array
-  // imagesForHtml = imagesForHtml.concat(ImagesOnScreen);
-  // imagesForHtml = imagesForHtml.concat(ImagesOnLastScreen);
 
   app.textContent = '';
 
@@ -320,17 +280,3 @@ try {
 } catch (error) {
   console.log(error);
 }
-
-// }
-// function tripleGen(){
-//   i=randImg();
-//   j=randImg();
-//   k=randImg();
-//   for (j; j == !i; j){
-//     j=randImg();
-//     for(k;k == !i||k==!j||j==!i; k){
-//       k=randImg();
-//
-//     return i,j,k;
-//   }
-// }
